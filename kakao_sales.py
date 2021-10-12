37980e8330d91079761eadbbed486a0dbe178977
looloolalaa <60506170+looloolalaa@users.noreply.github.com>
2021-10-12 16:00:24 +0900

    2021 kakao - 1-7. 매출 하락 최소화

---
+++ kakao_sales.py
@@ -0,0 +1,48 @@
+def solution(sales, links):
+    tree = {}
+    d = {}
+
+    def init_tree():
+        for link in links:
+            parent = link[0]
+            child = link[1]
+            if parent in tree:
+                tree[parent].append(child)
+            else:
+                tree[parent] = [child]
+
+    def sum_child(root):
+        return sum([min(d[child][0], d[child][1]) for child in tree[root]])
+
+    def get_d(root):
+        if root in tree:
+            children = tree[root]
+            for child in children:
+                if d[child][1] < d[child][0]:
+                    d[root] = [sum_child(root)]
+                    break
+            else:
+                diff = [d[child][1] - d[child][0] for child in children]
+                d[root] = [sum_child(root) + min(diff)]
+            d[root].append(sales[root-1] + sum_child(root))
+        else:
+            d[root] = [0]
+            d[root].append(sales[root-1])
+
+
+    def dfs(root):
+        if root in tree:
+            for child in tree[root]:
+                dfs(child)
+        get_d(root)
+
+    init_tree()
+    dfs(1)
+
+    return min(d[1])
+
+
+if __name__ == '__main__':
+    sales = [10, 10, 1, 1]
+    links = [[3,2], [4,3], [1,4]]
+    print(solution(sales, links))
