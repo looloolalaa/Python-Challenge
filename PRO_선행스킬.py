515a959834ed412c9d456de9fedbf1c12f834503
Kihyun <60506170+looloolalaa@users.noreply.github.com>
2022-04-15 10:14:02 +0900

    programmers 선행스킬

---
+++ PRO_선행스킬.py
@@ -0,0 +1,21 @@
+def solution(skill, skill_trees):
+
+    valid_skill = {''}
+    for l in range(len(skill)):
+        valid_skill.add(skill[:l+1])
+
+    def removed(s):
+        result = ''
+        for c in s:
+            if c in skill:
+                result += c
+        return result
+
+    count = 0
+    for s in skill_trees:
+        s = removed(s)
+        if s in valid_skill:
+            count += 1
+
+
+    return count
