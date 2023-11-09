0a7418cfec06afceee097a4fc83733aed9d3e5ea
Kihyun Lee <dlrlgus65@nate.com>
2023-11-10 01:06:46 +0900

    피보나치 dp

---
+++ PRO_2xn타일링.py
@@ -0,0 +1,10 @@
+# 피보나치 dp
+def solution(n):
+    dp = [0 for _ in range(n+1)]
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1_000_000_007
+    return dp[n]
+
+# 1 1 2 3 5 ..
